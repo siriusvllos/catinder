3be7d6b5ebef70722d9712cadb29ae6e5103bc72
Your Name <you@example.com>
2023-01-18 15:11:42 +0000

    feature add npm/express --- 1o commit do issue01

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// ---- FUNCTIONS ----
+
+app.get('/', function (req, res) {
+    res.send('OLA CATINDER <3')
+  })
+
+
