3a2af301a8b430b5195820781ab186af18965ba0
Sirius Victor <98984845+siriusvllos@users.noreply.github.com>
2023-03-10 16:20:00 -0300

    mongo + funcoes mongo

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(
   })
 );
 
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://localhost:44805/test";
+const client = new MongoClient(uri);
+
 // ---- FUNCTIONS ----
 
 function contarChamadas(obj){
@@ -39,6 +43,89 @@ app.post("/pass", async (req, res) => {
 
   var chamadas = await contarChamadas(passes);
   res.send("added");
+});
+
+// --- FUNCIOES ISSUE #7 ---
+
+async function listarUsuarios() {
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const database = client.db("vendas");
+    const usuarios = database.collection("usuarios");
+ 
+    const query = {};
+    var lista_usuarios = await usuarios.find(query).toArray();
+    return lista_usuarios;
+  } finally {
+    await client.close();
+  }
+ };
+ 
+ async function inserirNovoUsuario(novoUsuario) {
+  const client = new MongoClient(uri);
+ 
+  try {
+    await client.connect();
+    const database = client.db("vendas");
+    const usuarios = database.collection("usuarios");
+ 
+    await usuarios.insertOne(novoUsuario);
+  } finally {
+    await client.close(); 
+  }
+ };
+ 
+ async function deletarUsuario(nome) {
+  const client = new MongoClient(uri);
+ 
+  try {
+    await client.connect();
+    const database = client.db("vendas");
+    const usuarios = database.collection("usuarios");
+
+    await usuarios.remove({ nome: nome });
+  } finally {
+    await client.close();
+  }
+ };
+
+// --- URL'SES MONGO ISSUE #7 ---
+
+app.post("/users", async (req, res) => {
+  var novoUsuario = {
+    login: "",
+    email: "",
+    picture: ""
+  };
+  novoUsuario.login = req.param("nome de usuário");
+  novoUsuario.email = req.param("email");
+  novoUsuario.picture = req.param("foto de perfil");
+
+  console.log(`Tentei pegar os valores de ${novoUsuario}`);
+
+  var contaNova = await inserirNovoUsuario(novoUsuario);
+  res.send("added");
+});
+
+app.delete("/users", async (req, res) => {
+  var login = req.param("nome");
+
+  console.log(login);
+  console.log(email);
+
+ if (login != undefined) {
+    var adeusUsuario = await deletarUsuario(nome);
+    res.send("deleted");
+  } else if (login == undefined) {
+    res.send("ERRO: não conseguimos encontrar a sua conta");
+  }
+});
+
+app.get("/users", async (req, res) => {
+  var lista = await listarUsuarios();
+  res.send(lista);
 });
 
 // --- THE END ;) ---
